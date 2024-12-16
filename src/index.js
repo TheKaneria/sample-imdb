8fb44ccf11c3e24cd4a615109ee9cc1e1cf30e14
TheKaneria <zkaneria007@gmail.com>
2024-12-16 16:24:20 +0530

    latest code

--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ import "./index.css";
 import App from "./App";
 import ReactGA from "react-ga4";
 
-ReactGA.initialize("G-LEKFWNJE4L");
+ReactGA.initialize("G-E6FXZS62HG");
+ReactGA.send({ hitType: "pageview", page: window.location.pathname });
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
